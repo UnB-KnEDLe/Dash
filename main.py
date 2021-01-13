221a44304d7c00cd5eff4e483454b11af89f53e2
khalil09 <khalilcarsten@gmail.com>
2021-01-12 23:07:26 -0300

    ner and regex update

--- main.py
+++ main.py
@@ -33,12 +33,37 @@ correct_names = {
     'efetivos_exo': 'EFETIVOS EXO'
 }
 
+white_button_style = {'backgroundColor': 'white',
+                      'color': 'black',
+                      'height': '80px',
+                      'width': '150px',
+                      'borderRadius': '40px',
+                      'marginLeft': '2rem',
+                      'marginBottom': '2rem'
+                      }
+
+red_button_style = {'backgroundColor': '#6C63FF',
+                    'color': 'black',
+                    'height': '80px',
+                    'width': '150px',
+                    'borderRadius': '40px',
+                    'marginLeft': '2rem',
+                    'marginBottom': '2rem'
+                    }
+
 app.layout = html.Div([
     html.Link(href="https://fonts.googleapis.com/css2?family=Raleway&display=swap", rel="stylesheet"),
     html.Div([
         html.Div([
             html.H1('Extrator de PDF', className='card-title'),
             html.H2('Extrator de dados do diário oficial do DF', className='card-subtitle'),
+            html.H2('Selecione a forma de extração', className='card-subtitle'),
+            html.Div(id='type-extraction', style={'display': 'none'}),
+            html.Div([
+                html.Button('NER', id='ia-btn', n_clicks=0, style=white_button_style),
+                html.Button('REGEX', id='regex-btn', n_clicks=0, style=white_button_style)
+            ], style={'textAlign': 'center'}),
+            html.Div([]),
             dcc.Upload(
                 id='upload-data',
                 children=html.Div([
@@ -54,8 +79,21 @@ app.layout = html.Div([
     html.Img(src='assets/img/undraw_reviewed_docs_neeb 1.svg', className='background-img'),
 ])
 
+@app.callback([Output('ia-btn', 'style'), 
+              Output('regex-btn', 'style'),
+              Output('type-extraction', 'children')], 
+              [Input('ia-btn', 'n_clicks'), 
+              Input('regex-btn', 'n_clicks')])
+def change_button_style(n_clicks1, n_clicks2):
+    changed_id = [p['prop_id'] for p in dash.callback_context.triggered][0]
+    if 'ia-btn' in changed_id:
+        return [red_button_style, white_button_style, 'ner']
+    elif 'regex-btn' in changed_id:
+        return [white_button_style, red_button_style, 'regex']
+    else:
+        return [white_button_style, red_button_style, 'regex']
 
-def parse_contents(contents, filename, date):
+def parse_contents(contents, filename, date, type_extraction):
     global df_dict
     content_type, content_string = contents.split(',')
 
@@ -69,10 +107,9 @@ def parse_contents(contents, filename, date):
             temp_txt.write(text)
             temp_txt.close()
 
-            acts_dfs = ActsExtractor.get_all_df('tmp_txt.txt', 'regex')
+            acts_dfs = ActsExtractor.get_all_df('tmp_txt.txt', type_extraction)
 
     except Exception as e:
-        print(e)
         return html.Div([
             'There was an error processing this file.'
         ])
@@ -112,13 +149,14 @@ def parse_contents(contents, filename, date):
     return html.Div(list_of_tables)
 
 @app.callback(Output('output-data-upload', 'children'),
-              [Input('upload-data', 'contents')],
+              [Input('upload-data', 'contents'), 
+               Input('type-extraction', 'children')],
               [State('upload-data', 'filename'),
                State('upload-data', 'last_modified')])
-def update_output(list_of_contents, list_of_names, list_of_dates):
+def update_output(list_of_contents, type_extraction, list_of_names, list_of_dates):
     if list_of_contents is not None:
         children = [
-            parse_contents(c, n, d) for c, n, d in
+            parse_contents(c, n, d, type_extraction) for c, n, d in
             zip(list_of_contents, list_of_names, list_of_dates)]
         return children
 
@@ -128,7 +166,7 @@ def serve_static(file):
     return flask.send_file(file + ".csv", cache_timeout=1, attachment_filename=file+'.csv')
 
 if __name__ == '__main__':
-    app.run_server(host='0.0.0.0', debug=False)
+    app.run_server(host='0.0.0.0', debug=True)
     cache = Cache(app.server, config={
         'CACHE_TYPE': 'simple'
     })
