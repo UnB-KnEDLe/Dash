48f6b6b0b6cc2d41671f0b1b6002efd8f54784bd
notopoloko <amarals16@hotmail.com>
2021-08-02 10:36:48 -0300

    Test deploy

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -e
-scp -P ${PORT} -pr ${TRAVIS_BUILD_DIR} ${USER}@${SERVER}:~/${PROJECT}
+scp -P ${PORT} -pr ${TRAVIS_BUILD_DIR}/build ${USER}@${SERVER}:~/${PROJECT}
 command=$(sed "" <<< "cd ${PROJECT} && docker-compose up --build -d")
 # command=$(sed "" <<< "cd  && docker-compose up --build -d")
 ssh ${USER}@${SERVER} -p ${PORT} eval ${command}
