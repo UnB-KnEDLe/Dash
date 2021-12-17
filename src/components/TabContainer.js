95bd6595a6f8b188bba2000f215460ea1a18cc7b
Jônatas G <jonatasgeek@gmail.com>
2021-12-17 08:16:29 -0300

    adjust tabs

--- src/components/TabContainer.js
+++ src/components/TabContainer.js
@@ -5,11 +5,12 @@ import { BigCard } from '../styles/app'
 import ActsData, { Acts } from '../tabs/acts/Acts';
 import EntitiesData, { Entities } from '../tabs/entities/Entities';
 import SearchData, { Search } from '../tabs/search/Search';
-import AdvancedSearchData, { AdvancedSearch } from "../tabs/advancedSearch/AdvancedSearch";
+// import AdvancedSearchData, { AdvancedSearch } from "../tabs/advancedSearch/AdvancedSearch";
 
 export default function TabContainer(props) {
     const [activeTab, setActiveTab] = useState(0)
-    const tabs = [ActsData, EntitiesData, SearchData, AdvancedSearchData]
+    // const tabs = [ActsData, EntitiesData, SearchData, AdvancedSearchData]
+    const tabs = [ActsData, EntitiesData, SearchData]
 
     return (
         <BigCard>
@@ -29,9 +30,9 @@ export default function TabContainer(props) {
             { activeTab === 2 && (
                 <Search />
             ) }
-            { activeTab === 3 && (
+            {/* { activeTab === 3 && (
                 <AdvancedSearch />
-            ) }
+            ) } */}
         </BigCard>
     );
 }
