6ff809b0317bda038afaf022026593657c9a5403
Jônatas G <jonatasgeek@gmail.com>
2021-12-19 03:11:32 -0300

    new colors to acts

--- src/components/ActsComponent.js
+++ src/components/ActsComponent.js
@@ -4,17 +4,17 @@ import { useState } from "react"
 import { ActContainer } from "../styles/acts";
 
 const actsData = {
-    'abono':{title: 'Abono', color: "#090300"},
-    'aposentadoria':{title: 'Aposentadoria', color: "#db2d20"},
-    'cessoes':{title: 'Cessões', color: "#01a252"},
-    'efetivos_nome':{title: 'Nomeação - Efetivos', color: "#fded02"},
-    'efetivos_exo':{title: 'Exoneração - Efetivos', color: "#01a0e4"},
-    'exoneracao':{title: 'Exoneração', color: "#a16a94"},
-    'nomeacao':{title: 'Nomeação', color: "#b5e4f4"},
-    'retificacoes':{title: 'Retificações', color: "#e8bbd0"},
-    'reversoes':{title: 'Reversões', color: "#3a3432"},
-    'sem_efeito_aposentadoria':{title: 'Aposentadoria Tornada Sem Efeito', color: "#ff69b4"},
-    'substituicao':{title: 'Substituição', color: "#cdab53"},
+    'abono':{title: 'Abono', color: "#a3218e"},
+    'aposentadoria':{title: 'Aposentadoria', color: "#592f93"},
+    'cessoes':{title: 'Cessões', color: "#21409a"},
+    'efetivos_nome':{title: 'Nomeação - Efetivos', color: "#0465b2"},
+    'efetivos_exo':{title: 'Exoneração - Efetivos', color: "#00acac"},
+    'exoneracao':{title: 'Exoneração', color: "#03a45e"},
+    'nomeacao':{title: 'Nomeação', color: "#72bd46"},
+    'retificacoes':{title: 'Retificações', color: "#fef102"},
+    'reversoes':{title: 'Reversões', color: "#f8a51b"},
+    'sem_efeito_aposentadoria':{title: 'Aposentadoria Tornada Sem Efeito', color: "#f58225"},
+    'substituicao':{title: 'Substituição', color: "#ed403c"},
 }
 
 export default function ActsComponent({ actType, acts }) {
