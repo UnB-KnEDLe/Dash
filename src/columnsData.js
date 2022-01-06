223ad4646cd1f65a190d61ae76e7a3cbaf10267e
Jônatas G <jonatasgeek@gmail.com>
2022-01-06 20:59:39 -0300

    update columns names

--- src/columnsData.js
+++ src/columnsData.js
@@ -23,6 +23,16 @@ var columnsLabels = {
     fund_legal: 'Fundamento Legal',
     cod_matricula_ato: 'Código Matrícula',
     nome_ato: 'Nome',
+    cargo_substituto: 'Cargo do Substituto',
+    matricula_substituto: 'Matrícula do Substituto',
+    nome_substituto: 'Nome do Substituto',
+    cargo_objeto_substituicao: 'Cargo do Objeto da Substituição',
+    simbolo_objeto_substituicao: 'Símbolo do Objeto da Substituição',
+    hierarquia_lotacao: 'Hierarquia da Lotação',
+    data_inicial: 'Data Inicial',
+    data_final: 'Data Final',
+    simbolo_substituto: 'Símbolo do Substituto',
+    processo_sei: 'Processo SEI',
 }
 
 export const columnsReplace = columnName => {
