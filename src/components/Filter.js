75b6cc96ce2a5d05ef89da93f78c014ff0e3d5e2
Jônatas G <jonatasgeek@gmail.com>
2022-01-08 11:24:57 -0300

    adjust verticality

--- src/components/Filter.js
+++ src/components/Filter.js
@@ -21,9 +21,6 @@ export default function Filters() {
     const { sendError } = useError();
 
     const onSubmit = async (url) => {
-        let headingList = []
-        let contentList = []
-
         Object.keys(filters).forEach( label => {
             if (filters[label] !== '') {
                 url += `${label}=${filters[label]}&`;
@@ -32,50 +29,19 @@ export default function Filters() {
 
         setHeading([]);
         setContent({});
+        setLoading(true)
 
-        const response = await service(url)
+        const { headingList, contentList} = await service(url)
             .then( response => response)
             .catch( err => {
                 console.log(err)
                 setLoading(false)
-            })
-
-        if (Object.keys(response).length === 0) return [];
-
-        Array.from(Object.keys(response[0])).forEach( key => {
-            let item = response[0][key]
-            if (typeof item === 'string') {
-                headingList.push(key)
-            } else {
-                for (var itemKey in item) {
-                    if(itemKey === 'nome' && key !== 'pessoa') {
-                        headingList.push(key)
-                    }
-                    headingList.push(itemKey)
-                }
-            }
-        })
-        
-        response.forEach( item => {
-            let row = []
-            Object.keys(item).forEach( key => {
-                if(typeof item[key] === 'string') {
-                    row.push(item[key])
-                } else {
-                    for (var itemKey in item[key]) {
-                        row.push(item[key][itemKey])
-                    }
-                }
-            })
-            contentList.push(row)
-        } )
+            })        
 
         setHeading(headingList)
         setContent(contentList)
         
 		setStart(false);
-        setLoading(true)
-		await service(filters, baseUrl, setHeading, setContent, setLoading, sendError);
         setLoading(false)
 	}
 
--- src/services/searchService.js
+++ src/services/searchService.js
@@ -1,4 +1,7 @@
 export default async function service(url) {    
+    let headingList = []
+    let contentList = []
+
     var response = await fetch(url, {
         method: 'GET',
         timeout: 10000,
@@ -9,5 +12,35 @@ export default async function service(url) {
             return;
         } )
 
-    return response
+    if (Object.keys(response).length === 0) return [];
+
+    Array.from(Object.keys(response[0])).forEach( key => {
+        let item = response[0][key]
+        if (typeof item === 'string') {
+            headingList.push(key)
+        } else {
+            for (var itemKey in item) {
+                if(itemKey === 'nome' && key !== 'pessoa') {
+                    headingList.push(key)
+                }
+                headingList.push(itemKey)
+            }
+        }
+    })
+    
+    response.forEach( item => {
+        let row = []
+        Object.keys(item).forEach( key => {
+            if(typeof item[key] === 'string') {
+                row.push(item[key])
+            } else {
+                for (var itemKey in item[key]) {
+                    row.push(item[key][itemKey])
+                }
+            }
+        })
+        contentList.push(row)
+    } )
+
+    return { headingList, contentList }
 }
