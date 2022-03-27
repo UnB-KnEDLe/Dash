9400e9911acbdc3927cf70351d75225613b8893d
Jônatas G <jonatasgeek@gmail.com>
2022-03-27 15:43:17 -0300

    Add a input to change cypher

--- src/tabs/neo4j/neo4j.js
+++ src/tabs/neo4j/neo4j.js
@@ -1,29 +1,37 @@
-
-import React, { useEffect, useRef } from "react";
+import React, { useEffect, useState, useRef } from "react";
 import Neovis from "neovis.js/dist/neovis.js";
 
-const NeoGraph = (props) => {
-  const visRef = useRef();
+import "./style.css"
 
-  useEffect(() => {
-    const config = {
-      container_id: visRef.current.id,
-      server_url: "neo4j://164.41.76.30:8080",
-      server_user: "neo4j",
-      server_password: "nido@CIC2021",
-      initial_cypher:
-        "match (p:Pessoa) where p.nome contains('MARIA') return p limit 10",
-    };
-    const vis = new Neovis(config);
-    vis.render();
-  }, []);
+const NeoGraph = () => {
+    const visRef = useRef();
+    const [cypherText, setCypherText] = useState("match (p:Pessoa) where p.nome contains('MARIA') return p limit 10");
+    const [cypher, setCypher] = useState(cypherText);
 
-  return (
-    <div
-      id={"hehe"}
-      ref={visRef}
-    />
-  );
+    const onRunCypher = () => setCypher(cypherText);
+
+    useEffect(() => {
+        const config = {
+            container_id: visRef.current.id,
+            server_url: "neo4j://164.41.76.30:8080",
+            server_user: "neo4j",
+            server_password: "nido@CIC2021",
+            initial_cypher: cypher,
+        };
+        const vis = new Neovis(config);
+        vis.render();
+    }, [cypher]);
+
+    return (
+        <div className="container">
+            <div className="vis-input">
+                <label>Query: </label>
+                <input className="search_input" value={cypherText} onChange={(e) => setCypherText(e.target.value)} />
+                <button class="btn" onClick={onRunCypher}>Executar</button>
+            </div>
+            <div id="graph" ref={visRef} />
+        </div>
+    );
 };
 
 export default NeoGraph;
