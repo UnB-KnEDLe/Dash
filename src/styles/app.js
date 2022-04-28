5a5157fe2aeaa12abc02c0e7ec0dbea5c54417e4
Jônatas G <jonatasgeek@gmail.com>
2022-04-27 21:50:11 -0300

    Fix hover inconsistency

--- src/styles/app.js
+++ src/styles/app.js
@@ -208,23 +208,4 @@ export const Card = styled.div`
       default:
         return "none";
   }}};
-
-  :hover {
-    box-shadow: ${(props) => {
-      if(!props.hover) return "none";
-      switch (props.shadow) {
-        case 0:
-          return "0 1px 3px rgba(0,0,0,0.12), 0 1px 2px rgba(0,0,0,0.24);"
-        case 1:
-          return "0 3px 6px rgba(0,0,0,0.16), 0 3px 6px rgba(0,0,0,0.23);"
-        case 2: 
-          return "0 10px 20px rgba(0,0,0,0.19), 0 6px 6px rgba(0,0,0,0.23);"
-        case 3:
-          return "0 14px 28px rgba(0,0,0,0.25), 0 10px 10px rgba(0,0,0,0.22);"
-        case 4:
-          return "0 19px 38px rgba(0,0,0,0.30), 0 15px 12px rgba(0,0,0,0.22);"
-        default:
-          return "0 1px 3px rgba(0,0,0,0.12), 0 1px 2px rgba(0,0,0,0.24);"
-      }
-    }}
 `;
