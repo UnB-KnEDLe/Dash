8eac2429af4cbb8d3e244c6f6055c45f41575209
dansousamelo <daniel_sousa.unb@outlook.com>
2022-06-20 15:20:18 -0300

    chore: refact table

--- src/pages/search.tsx
+++ src/pages/search.tsx
@@ -6,6 +6,7 @@ import { Header } from "../components/Header";
 import SearchSelectActType from '../components/SearchSelectActType';
 import SearchSetInput from '../components/SearchSetInput';
 import { Sidebar } from "../components/Sidebar";
+import HeadingTwo from '../components/Typography/HeadingTwo';
 
 export default function Search(){
   const [showInputElements, setShowInputElements] = useState<string[]>([]);
@@ -44,83 +45,95 @@ export default function Search(){
 
             </SimpleGrid>
             <Divider text="resultados"/>
-            <Table variant="striped" colorScheme="blackAlpha">
-              <Thead bgColor="pallete.darkPrimary">
-                <Tr>
-                  <Th fontWeight="bold" color="pallete.background" px="4px" maxWidth="80px" pl="28px">SEI</Th>
-                  <Th fontWeight="bold" color="pallete.background" px="4px" maxWidth="80px">Nome</Th>
-                  <Th fontWeight="bold" color="pallete.background">Matrícula</Th>
-                  <Th fontWeight="bold" color="pallete.background">Órgão</Th>
-                  <Th fontWeight="bold" color="pallete.background">DODF Número</Th>
-                  <Th fontWeight="bold" color="pallete.background">DODF Nome</Th>
-                  <Th fontWeight="bold" color="pallete.background">DATA DODF</Th>
-                  <Th fontWeight="bold" color="pallete.background">Quadro</Th>
-                  <Th fontWeight="bold" color="pallete.background">Padrão</Th>
-                  <Th fontWeight="bold" color="pallete.background">Cargo</Th>
-                  <Th fontWeight="bold" color="pallete.background">Efetivo</Th>
-                  <Th fontWeight="bold" color="pallete.background">Classe</Th>
-                </Tr>
-              </Thead>
-              <Tbody fontWeight="500" bgColor="pallete.sidebarBackground">
-                <Tr >
-                  <Td pl="28px"px="4px" maxWidth="80px">060.00047003/2017-40</Td>
-                  <Td pl="28px" px="4px" maxWidth="80px">LUIZ SERGIO MAGALHAES BRAGA</Td>
-                  <Td></Td>
-                  <Td></Td>
-                  <Td>001</Td>
-                  <Td>DODF 001 02-01-2018.pdf</Td>
-                  <Td>02/01/2018</Td>
-                  <Td>Quadro de Pessoal da Secretaria de Estado de Saude do Distrito Federal'</Td>
-                  <Td>Padrao IV', 'Padrao V</Td>
-                  <Td>Medico Cirurgia Geral' 'Tecnico em Saude</Td>
-                  <Td></Td>
-                  <Td>Classe Especial'</Td>
-                </Tr>
-                <Tr>
-                  <Td pl="28px" px="4px" maxWidth="80px">060.00047003/2017-40</Td>
-                  <Td pl="28px" px="4px" maxWidth="80px">LUIZ SERGIO MAGALHAES BRAGA</Td>
-                  <Td></Td>
-                  <Td></Td>
-                  <Td>001</Td>
-                  <Td>DODF 001 02-01-2018.pdf</Td>
-                  <Td>02/01/2018</Td>
-                  <Td>Quadro de Pessoal da Secretaria de Estado de Saude do Distrito Federal'</Td>
-                  <Td>Padrao IV', 'Padrao V</Td>
-                  <Td>Medico Cirurgia Geral' 'Tecnico em Saude</Td>
-                  <Td></Td>
-                  <Td>Classe Especial'</Td>
-                </Tr>
-                <Tr>
-                  <Td pl="28px" px="4px" maxWidth="80px">060.00047003/2017-40</Td>
-                  <Td pl="28px" px="4px" maxWidth="80px">LUIZ SERGIO MAGALHAES BRAGA</Td>
-                  <Td></Td>
-                  <Td></Td>
-                  <Td>001</Td>
-                  <Td>DODF 001 02-01-2018.pdf</Td>
-                  <Td>02/01/2018</Td>
-                  <Td>Quadro de Pessoal da Secretaria de Estado de Saude do Distrito Federal'</Td>
-                  <Td>Padrao IV', 'Padrao V</Td>
-                  <Td>Medico Cirurgia Geral' 'Tecnico em Saude</Td>
-                  <Td></Td>
-                  <Td>Classe Especial'</Td>
-                </Tr>
-                <Tr>
-                  <Td pl="28px" px="4px" maxWidth="80px">060.00047003/2017-40</Td>
-                  <Td pl="28px" px="4px" maxWidth="80px">LUIZ SERGIO MAGALHAES BRAGA</Td>
-                  <Td></Td>
-                  <Td></Td>
-                  <Td>001</Td>
-                  <Td>DODF 001 02-01-2018.pdf</Td>
-                  <Td>02/01/2018</Td>
-                  <Td>Quadro de Pessoal da Secretaria de Estado de Saude do Distrito Federal'</Td>
-                  <Td>Padrao IV', 'Padrao V</Td>
-                  <Td>Medico Cirurgia Geral' 'Tecnico em Saude</Td>
-                  <Td></Td>
-                  <Td>Classe Especial'</Td>
-                </Tr>
-              </Tbody>
-            </Table>
-
+            <Box bgColor="pallete.cardBackground" borderRadius='0.25rem' padding="3rem"> 
+              <HeadingTwo marginBottom="1.5rem" headingTwoText='Atos de Aposentadoria'/>
+            
+              <Box
+                bgColor="pallete.cardBackground"
+                border="1px solid" 
+                borderColor="pallete.secondary" 
+                borderRadius="md"
+              >
+                <Table variant="striped" colorScheme="facebook">
+                  <Thead bgColor="pallete.secondary">
+                    <Tr>
+                      <Th fontWeight="bold" color="pallete.background" px="4px" maxWidth="80px" pl="28px">SEI</Th>
+                      <Th fontWeight="bold" color="pallete.background" px="4px" maxWidth="80px" pl="28px">Nome</Th>
+                      <Th fontWeight="bold" color="pallete.background">Matrícula</Th>
+                      <Th fontWeight="bold" color="pallete.background">Órgão</Th>
+                      <Th fontWeight="bold" color="pallete.background">DODF Número</Th>
+                      <Th fontWeight="bold" color="pallete.background">DODF Nome</Th>
+                      <Th fontWeight="bold" color="pallete.background">DATA DODF</Th>
+                      <Th fontWeight="bold" color="pallete.background">Quadro</Th>
+                      <Th fontWeight="bold" color="pallete.background">Padrão</Th>
+                      <Th fontWeight="bold" color="pallete.background">Cargo</Th>
+                      <Th fontWeight="bold" color="pallete.background">Efetivo</Th>
+                      <Th fontWeight="bold" color="pallete.background">Classe</Th>
+                    </Tr>
+                  </Thead>
+                  
+                  <Tbody fontWeight="500">
+                    <Tr>
+                      <Td pl="28px"px="4px" maxWidth="80px">060.00047003/2017-40</Td>
+                      <Td pl="28px" px="4px" maxWidth="80px">LUIZ SERGIO MAGALHAES BRAGA</Td>
+                      <Td></Td>
+                      <Td></Td>
+                      <Td>001</Td>
+                      <Td>DODF 001 02-01-2018.pdf</Td>
+                      <Td>02/01/2018</Td>
+                      <Td>Quadro de Pessoal da Secretaria de Estado de Saude do Distrito Federal'</Td>
+                      <Td>Padrao IV', 'Padrao V</Td>
+                      <Td>Medico Cirurgia Geral' 'Tecnico em Saude</Td>
+                      <Td></Td>
+                      <Td>Classe Especial'</Td>
+                    </Tr>
+                    <Tr>
+                      <Td pl="28px" px="4px" maxWidth="80px">060.00047003/2017-40</Td>
+                      <Td pl="28px" px="4px" maxWidth="80px">LUIZ SERGIO MAGALHAES BRAGA</Td>
+                      <Td></Td>
+                      <Td></Td>
+                      <Td>001</Td>
+                      <Td>DODF 001 02-01-2018.pdf</Td>
+                      <Td>02/01/2018</Td>
+                      <Td>Quadro de Pessoal da Secretaria de Estado de Saude do Distrito Federal'</Td>
+                      <Td>Padrao IV', 'Padrao V</Td>
+                      <Td>Medico Cirurgia Geral' 'Tecnico em Saude</Td>
+                      <Td></Td>
+                      <Td>Classe Especial'</Td>
+                    </Tr>
+                    <Tr>
+                      <Td pl="28px" px="4px" maxWidth="80px">060.00047003/2017-40</Td>
+                      <Td pl="28px" px="4px" maxWidth="80px">LUIZ SERGIO MAGALHAES BRAGA</Td>
+                      <Td></Td>
+                      <Td></Td>
+                      <Td>001</Td>
+                      <Td>DODF 001 02-01-2018.pdf</Td>
+                      <Td>02/01/2018</Td>
+                      <Td>Quadro de Pessoal da Secretaria de Estado de Saude do Distrito Federal'</Td>
+                      <Td>Padrao IV', 'Padrao V</Td>
+                      <Td>Medico Cirurgia Geral' 'Tecnico em Saude</Td>
+                      <Td></Td>
+                      <Td>Classe Especial'</Td>
+                    </Tr>
+                    <Tr>
+                      <Td pl="28px" px="4px" maxWidth="80px">060.00047003/2017-40</Td>
+                      <Td pl="28px" px="4px" maxWidth="80px">LUIZ SERGIO MAGALHAES BRAGA</Td>
+                      <Td></Td>
+                      <Td></Td>
+                      <Td>001</Td>
+                      <Td>DODF 001 02-01-2018.pdf</Td>
+                      <Td>02/01/2018</Td>
+                      <Td>Quadro de Pessoal da Secretaria de Estado de Saude do Distrito Federal'</Td>
+                      <Td>Padrao IV', 'Padrao V</Td>
+                      <Td>Medico Cirurgia Geral' 'Tecnico em Saude</Td>
+                      <Td></Td>
+                      <Td>Classe Especial'</Td>
+                    </Tr>
+                  </Tbody>
+                  
+                </Table>
+              </Box>
+            </Box>
         </Flex>
       </Flex>
     </Flex>
