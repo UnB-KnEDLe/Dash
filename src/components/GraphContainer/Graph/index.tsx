2133e34ed81545b103ae19a551741bdc6124ba87
Lary15 <lari1415@hotmail.com>
2022-07-10 18:30:17 -0300

    Add - Graph Popup

--- src/components/GraphContainer/Graph/index.tsx
+++ src/components/GraphContainer/Graph/index.tsx
@@ -1,21 +1,27 @@
-import { useLayoutEffect, useRef } from 'react';
+import { Dispatch, SetStateAction, useLayoutEffect, useRef } from 'react';
 import { Flex } from '@chakra-ui/react';
-import NeoVis, { NEOVIS_DEFAULT_CONFIG, NeoVisEvents } from 'neovis.js';
+import NeoVis, { NeoVisEvents } from 'neovis.js';
+import { PopupContentType } from "../GraphPopup";
 import { useUser } from '../../../hooks/user';
 
 interface GraphProps {
-  username?: string;
-  password?: string;
   cypher: string;
+  setPopupContent : Dispatch<SetStateAction<PopupContentType>>;
+  setOpenPopup : Dispatch<SetStateAction<boolean>>;
 }
 
-function Graph({ cypher }: GraphProps) {
+function Graph({ cypher, setPopupContent, setOpenPopup }: GraphProps) {
   const visRef = useRef<HTMLDivElement>();
   const { user} = useUser();
 
-  const handleCompleted = (vis: any) => {
-    console.log(vis.nodes);
+  const handleClick = (data: any) => {
+    setPopupContent({
+      title: data?.raw?.type || data?.raw?.labels[0],
+      properties: data?.raw?.properties
+    });
+    setOpenPopup(true);
   };
+
   useLayoutEffect(() => {
     const config = {
       containerId: visRef?.current?.id,
@@ -25,14 +31,15 @@ function Graph({ cypher }: GraphProps) {
         serverUrl: 'neo4j://164.41.76.30:8080',
       },
       initialCypher: cypher,
-      [NEOVIS_DEFAULT_CONFIG]: {
-        title: 'nome',
-      },
     };
 
     let vis = new NeoVis(config);
+
     vis.render();
-    vis.registerOnEvent(NeoVisEvents.ClickNodeEvent, () => handleCompleted(vis));
+
+    vis.registerOnEvent(NeoVisEvents.ClickNodeEvent, (data) => handleClick(data.node));
+    vis.registerOnEvent(NeoVisEvents.ClickEdgeEvent, (data) => handleClick(data.edge));
+
   }, [cypher]);
 
   return <Flex ref={visRef} id="viz" w="100%" />;
