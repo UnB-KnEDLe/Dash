14b5dd48f638d47ee445b407af55623d8ca60820
danveras <daniel.veras@wisereducacao.com>
2022-09-21 20:54:16 -0300

    feat: adjust timeline lib

--- src/components/TimelineComponent/Element/index.tsx
+++ src/components/TimelineComponent/Element/index.tsx
@@ -1,7 +1,5 @@
 import { VerticalTimelineElement } from 'react-vertical-timeline-component';
-import { Flex } from '@chakra-ui/react';
-import HeadingTwo from '../../Typography/HeadingTwo';
-import { AiOutlineFile } from 'react-icons/ai';
+import { FcDocument } from 'react-icons/fc';
 
 export interface TimelineElementProps {
     date: string;
@@ -10,38 +8,20 @@ export interface TimelineElementProps {
 }
 
 export default function TimelineElement({date, title, text}: TimelineElementProps) {
-    const iconStyle = {
-        display: 'flex',
-        position: 'absolute',
-        left: 'calc(50% - 2rem)',
-        padding: '1rem',
-        borderRadius: '50%',
-        border: '3px solid #e3e3e3',
-        background: '#17385D',
-        color: '#EDEFF5',
-        alignSelf: 'center'
-    }
-    
-    const contentStyle = {
-        display: 'flex',
-        justifyContent: 'center',
-        alignContent: 'center',
-    }
 
     return (
-        <Flex
-            as={VerticalTimelineElement}
-            date={date}
-            order='revert'
-            justifyContent='flex-end'
-            contentArrowStyle={{ borderRight: '3px solid #D9D9D9' }}
-            icon={<AiOutlineFile size='1.5rem'/>}
-            gap='1rem'
-            iconStyle={iconStyle}
-            contentStyle={contentStyle}
-        >
-            <HeadingTwo headingTwoText={title} />
-            {text}
-        </Flex>
+        <VerticalTimelineElement
+        className="vertical-timeline-element--work"
+        contentStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
+        contentArrowStyle={{ borderRight: '7px solid  rgb(33, 150, 243)' }}
+        date={date}
+        iconStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
+        icon={<FcDocument />}
+      >
+        <h3 className="vertical-timeline-element-title">{title}</h3>
+        <p>
+          {text}
+        </p>
+      </VerticalTimelineElement>
     )
 }
--- src/components/TimelineComponent/index.tsx
+++ src/components/TimelineComponent/index.tsx
@@ -1,7 +1,9 @@
-import { VerticalTimeline } from 'react-vertical-timeline-component';
+import { VerticalTimeline, VerticalTimelineElement }  from 'react-vertical-timeline-component';
+import 'react-vertical-timeline-component/style.min.css';
 import TimelineElement, {TimelineElementProps} from './Element';
 import HeadingTwo from '../Typography/HeadingTwo';
 import { Flex } from '@chakra-ui/react';
+import { FcDocument } from 'react-icons/fc';
 
 interface TimelineComponentProps {
     secretary: string;
@@ -11,26 +13,98 @@ interface TimelineComponentProps {
 
 export default function TimelineComponent({secretary, timelineElements}: TimelineComponentProps) {
     return (
-        <Flex flexDir='column' gap='1rem'>
-            <HeadingTwo textAlign='center' color='pallete.darkPrimary' headingTwoText={secretary} />
-            
-            <Flex
-                as={VerticalTimeline}
-                position='relative'
-                gap='2rem'
-                flexDir='column'
-                pb='1rem'
-                mb='1rem'
-                borderBottom='1px'
-            >
-                { timelineElements.map( ({date, title, text}) => (
-                    <TimelineElement
-                        date={date}
-                        title={title}
-                        text={text}
-                    />
-                )) }
-            </Flex>
-        </Flex>
+        <VerticalTimeline>
+        <VerticalTimelineElement
+          className="vertical-timeline-element--work"
+          contentStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
+          contentArrowStyle={{ borderRight: '7px solid  rgb(33, 150, 243)' }}
+          date="2011 - present"
+          iconStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
+          icon={<FcDocument />}
+        >
+          <h3 className="vertical-timeline-element-title">Creative Director</h3>
+          <h4 className="vertical-timeline-element-subtitle">Miami, FL</h4>
+          <p>
+            Creative Direction, User Experience, Visual Design, Project Management, Team Leading
+          </p>
+        </VerticalTimelineElement>
+        <VerticalTimelineElement
+          className="vertical-timeline-element--work"
+          date="2010 - 2011"
+          iconStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
+          icon={<FcDocument />}
+        >
+          <h3 className="vertical-timeline-element-title">Art Director</h3>
+          <h4 className="vertical-timeline-element-subtitle">San Francisco, CA</h4>
+          <p>
+            Creative Direction, User Experience, Visual Design, SEO, Online Marketing
+          </p>
+        </VerticalTimelineElement>
+        <VerticalTimelineElement
+          className="vertical-timeline-element--work"
+          date="2008 - 2010"
+          iconStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
+          icon={<FcDocument />}
+        >
+          <h3 className="vertical-timeline-element-title">Web Designer</h3>
+          <h4 className="vertical-timeline-element-subtitle">Los Angeles, CA</h4>
+          <p>
+            User Experience, Visual Design
+          </p>
+        </VerticalTimelineElement>
+        <VerticalTimelineElement
+          className="vertical-timeline-element--work"
+          date="2006 - 2008"
+          iconStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
+          icon={<FcDocument />}
+        >
+          <h3 className="vertical-timeline-element-title">Web Designer</h3>
+          <h4 className="vertical-timeline-element-subtitle">San Francisco, CA</h4>
+          <p>
+            User Experience, Visual Design
+          </p>
+        </VerticalTimelineElement>
+        <VerticalTimelineElement
+          className="vertical-timeline-element--education"
+          date="April 2013"
+          iconStyle={{ background: 'rgb(233, 30, 99)', color: '#fff' }}
+          icon={<FcDocument />}
+        >
+          <h3 className="vertical-timeline-element-title">Content Marketing for Web, Mobile and Social Media</h3>
+          <h4 className="vertical-timeline-element-subtitle">Online Course</h4>
+          <p>
+            Strategy, Social Media
+          </p>
+        </VerticalTimelineElement>
+        <VerticalTimelineElement
+          className="vertical-timeline-element--education"
+          date="November 2012"
+          iconStyle={{ background: 'rgb(233, 30, 99)', color: '#fff' }}
+          icon={<FcDocument />}
+        >
+          <h3 className="vertical-timeline-element-title">Agile Development Scrum Master</h3>
+          <h4 className="vertical-timeline-element-subtitle">Certification</h4>
+          <p>
+            Creative Direction, User Experience, Visual Design
+          </p>
+        </VerticalTimelineElement>
+        <VerticalTimelineElement
+          className="vertical-timeline-element--education"
+          date="2002 - 2006"
+          iconStyle={{ background: 'rgb(233, 30, 99)', color: '#fff' }}
+          icon={<FcDocument />}
+        >
+          <h3 className="vertical-timeline-element-title">Bachelor of Science in Interactive Digital Media Visual Imaging</h3>
+          <h4 className="vertical-timeline-element-subtitle">Bachelor Degree</h4>
+          <p>
+            Creative Direction, Visual Design
+          </p>
+        </VerticalTimelineElement>
+        <VerticalTimelineElement
+          iconStyle={{ background: 'rgb(16, 204, 82)', color: '#fff' }}
+          icon={<FcDocument />}
+        />
+      </VerticalTimeline>
+      
     )
 }
