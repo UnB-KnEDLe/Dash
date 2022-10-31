dfe2f192a6f5df88d3e3ee48037d82bd5f9548c8
Jônatas Gomes <jonatasgeek@gmail.com>
2022-10-31 18:25:31 -0300

    Set mocked elements to hook

---
+++ src/hooks/timeline.tsx
@@ -0,0 +1,84 @@
+export const elements = {
+	'SECRETARIA DE ESTADO DE TRABALHO': {
+		'AVISO DE PRORROGAÇÃO DE PRAZOS': [
+			{
+				datePublication: "10/10/2022",
+				text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum eligendi qui laborum, autem nam assumenda repudiandae animi atque aspernatur asperiores dolorum culpa. Dolore, delectus.'
+			},
+			{
+				datePublication: "10/10/2022",
+				text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum eligendi qui laborum, autem nam assumenda repudiandae animi atque aspernatur asperiores dolorum culpa. Dolore, delectus.'
+			},
+			{
+				datePublication: "10/10/2022",
+				text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum eligendi qui laborum, autem nam assumenda repudiandae animi atque aspernatur asperiores dolorum culpa. Dolore, delectus.'
+			},
+			{
+				datePublication: "10/10/2022",
+				text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum eligendi qui laborum, autem nam assumenda repudiandae animi atque aspernatur asperiores dolorum culpa. Dolore, delectus.'
+			},
+			{
+				datePublication: "10/10/2022",
+				text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum eligendi qui laborum, autem nam assumenda repudiandae animi atque aspernatur asperiores dolorum culpa. Dolore, delectus.'
+			},
+			{
+				datePublication: "10/10/2022",
+				text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum eligendi qui laborum, autem nam assumenda repudiandae animi atque aspernatur asperiores dolorum culpa. Dolore, delectus.'
+			},
+		]
+	},
+	'SECRETARIA DE ESTADO DE SAUDE': {
+		'EDITAL DE CHAMAMENTO PÚBLICO Nº 16/2021': [
+			{
+				datePublication: "10/10/2022",
+				text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum eligendi qui laborum, autem nam assumenda repudiandae animi atque aspernatur asperiores dolorum culpa. Dolore, delectus.'
+			},
+			{
+				datePublication: "10/10/2022",
+				text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum eligendi qui laborum, autem nam assumenda repudiandae animi atque aspernatur asperiores dolorum culpa. Dolore, delectus.'
+			},
+			{
+				datePublication: "10/10/2022",
+				text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum eligendi qui laborum, autem nam assumenda repudiandae animi atque aspernatur asperiores dolorum culpa. Dolore, delectus.'
+			},
+			{
+				datePublication: "10/10/2022",
+				text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum eligendi qui laborum, autem nam assumenda repudiandae animi atque aspernatur asperiores dolorum culpa. Dolore, delectus.'
+			},
+			{
+				datePublication: "10/10/2022",
+				text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum eligendi qui laborum, autem nam assumenda repudiandae animi atque aspernatur asperiores dolorum culpa. Dolore, delectus.'
+			},
+			{
+				datePublication: "10/10/2022",
+				text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum eligendi qui laborum, autem nam assumenda repudiandae animi atque aspernatur asperiores dolorum culpa. Dolore, delectus.'
+			},
+		],
+		'EXTRATO DO TERMO DE FOMENTO Nº 05/2021': [
+			{
+				datePublication: "10/10/2022",
+				text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum eligendi qui laborum, autem nam assumenda repudiandae animi atque aspernatur asperiores dolorum culpa. Dolore, delectus.'
+			},
+			{
+				datePublication: "10/10/2022",
+				text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum eligendi qui laborum, autem nam assumenda repudiandae animi atque aspernatur asperiores dolorum culpa. Dolore, delectus.'
+			},
+			{
+				datePublication: "10/10/2022",
+				text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum eligendi qui laborum, autem nam assumenda repudiandae animi atque aspernatur asperiores dolorum culpa. Dolore, delectus.'
+			},
+			{
+				datePublication: "10/10/2022",
+				text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum eligendi qui laborum, autem nam assumenda repudiandae animi atque aspernatur asperiores dolorum culpa. Dolore, delectus.'
+			},
+			{
+				datePublication: "10/10/2022",
+				text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum eligendi qui laborum, autem nam assumenda repudiandae animi atque aspernatur asperiores dolorum culpa. Dolore, delectus.'
+			},
+			{
+				datePublication: "10/10/2022",
+				text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum eligendi qui laborum, autem nam assumenda repudiandae animi atque aspernatur asperiores dolorum culpa. Dolore, delectus.'
+			},
+		],
+	}
+}
