353436b13746b2173f5d8bf5e720894c395ba162
Jônatas Gomes <jonatasgeek@gmail.com>
2022-11-07 18:53:20 -0300

    create hook

--- src/hooks/timeline.tsx
+++ src/hooks/timeline.tsx
@@ -1,84 +1,79 @@
-export const elements = {
-	'SECRETARIA DE ESTADO DE TRABALHO': {
-		'AVISO DE PRORROGAÇÃO DE PRAZOS': [
-			{
-				datePublication: "10/10/2022",
-				text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum eligendi qui laborum, autem nam assumenda repudiandae animi atque aspernatur asperiores dolorum culpa. Dolore, delectus.'
-			},
-			{
-				datePublication: "10/10/2022",
-				text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum eligendi qui laborum, autem nam assumenda repudiandae animi atque aspernatur asperiores dolorum culpa. Dolore, delectus.'
-			},
-			{
-				datePublication: "10/10/2022",
-				text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum eligendi qui laborum, autem nam assumenda repudiandae animi atque aspernatur asperiores dolorum culpa. Dolore, delectus.'
-			},
-			{
-				datePublication: "10/10/2022",
-				text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum eligendi qui laborum, autem nam assumenda repudiandae animi atque aspernatur asperiores dolorum culpa. Dolore, delectus.'
-			},
-			{
-				datePublication: "10/10/2022",
-				text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum eligendi qui laborum, autem nam assumenda repudiandae animi atque aspernatur asperiores dolorum culpa. Dolore, delectus.'
-			},
-			{
-				datePublication: "10/10/2022",
-				text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum eligendi qui laborum, autem nam assumenda repudiandae animi atque aspernatur asperiores dolorum culpa. Dolore, delectus.'
-			},
-		]
-	},
-	'SECRETARIA DE ESTADO DE SAUDE': {
-		'EDITAL DE CHAMAMENTO PÚBLICO Nº 16/2021': [
-			{
-				datePublication: "10/10/2022",
-				text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum eligendi qui laborum, autem nam assumenda repudiandae animi atque aspernatur asperiores dolorum culpa. Dolore, delectus.'
-			},
-			{
-				datePublication: "10/10/2022",
-				text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum eligendi qui laborum, autem nam assumenda repudiandae animi atque aspernatur asperiores dolorum culpa. Dolore, delectus.'
-			},
-			{
-				datePublication: "10/10/2022",
-				text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum eligendi qui laborum, autem nam assumenda repudiandae animi atque aspernatur asperiores dolorum culpa. Dolore, delectus.'
-			},
-			{
-				datePublication: "10/10/2022",
-				text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum eligendi qui laborum, autem nam assumenda repudiandae animi atque aspernatur asperiores dolorum culpa. Dolore, delectus.'
-			},
-			{
-				datePublication: "10/10/2022",
-				text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum eligendi qui laborum, autem nam assumenda repudiandae animi atque aspernatur asperiores dolorum culpa. Dolore, delectus.'
-			},
-			{
-				datePublication: "10/10/2022",
-				text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum eligendi qui laborum, autem nam assumenda repudiandae animi atque aspernatur asperiores dolorum culpa. Dolore, delectus.'
-			},
-		],
-		'EXTRATO DO TERMO DE FOMENTO Nº 05/2021': [
-			{
-				datePublication: "10/10/2022",
-				text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum eligendi qui laborum, autem nam assumenda repudiandae animi atque aspernatur asperiores dolorum culpa. Dolore, delectus.'
-			},
-			{
-				datePublication: "10/10/2022",
-				text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum eligendi qui laborum, autem nam assumenda repudiandae animi atque aspernatur asperiores dolorum culpa. Dolore, delectus.'
-			},
-			{
-				datePublication: "10/10/2022",
-				text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum eligendi qui laborum, autem nam assumenda repudiandae animi atque aspernatur asperiores dolorum culpa. Dolore, delectus.'
-			},
-			{
-				datePublication: "10/10/2022",
-				text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum eligendi qui laborum, autem nam assumenda repudiandae animi atque aspernatur asperiores dolorum culpa. Dolore, delectus.'
-			},
-			{
-				datePublication: "10/10/2022",
-				text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum eligendi qui laborum, autem nam assumenda repudiandae animi atque aspernatur asperiores dolorum culpa. Dolore, delectus.'
-			},
-			{
-				datePublication: "10/10/2022",
-				text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum eligendi qui laborum, autem nam assumenda repudiandae animi atque aspernatur asperiores dolorum culpa. Dolore, delectus.'
-			},
-		],
-	}
+import React, { useState, useCallback, createContext, useEffect, useContext } from 'react'
+
+interface TimelineContextData {
+	secretary: any[];
+	actTypes: string[];
+	acts: string[];
+	setSecretary: React.Dispatch<React.SetStateAction<any[]>>;
+	setActTypes: React.Dispatch<React.SetStateAction<any[]>>;
+	setActs: React.Dispatch<React.SetStateAction<any[]>>;
+	handleSelectedSecretary: (secretary: string) => void;
+	handleSelectedActTypes: (actType: any) => void;
+	resetAllFields: () => void;
 }
+const elements = {}
+const TimelineContext = createContext<TimelineContextData>({} as TimelineContextData)
+
+type TimelineProviderProps = {
+	children: React.ReactNode;
+}
+
+function TimelineProvider({children}: TimelineProviderProps ): JSX.Element {
+	const [secretary, setSecretary] = useState([]);
+	const [actTypes, setActTypes] = useState([]);
+	const [acts, setActs] = useState([]);
+
+	const handleSelectedSecretary = useCallback((secretary: string) => {
+		setSecretary(secretary);
+		setActTypes([]);
+	}, []);
+
+	const handleSelectedActTypes = useCallback((actType: any) => {
+		let {checked, value} = actType;
+		let newActTypes = actTypes;
+		if(checked && !newActTypes.includes(value)) {
+			newActTypes.push(value);
+			setActTypes(newActTypes);
+			return;
+		}
+		newActTypes = newActTypes.filter(act => act != value);
+		setActTypes(newActTypes);
+	}, [])
+
+	useEffect( () => {
+		let newActs = [];
+		actTypes.forEach(type => {
+			newActs.push(...elements[secretary][type])
+		})
+		setActs(newActs);
+	}, [])
+
+	return (
+		<TimelineContext.Provider
+			value={{
+				secretary,
+				actTypes,
+				acts,
+				setSecretary,
+				setActTypes,
+				setActs,
+				handleSelectedSecretary,
+				handleSelectedActTypes,
+			}}
+		>
+			{children}
+		</TimelineContext.Provider>
+	)
+}
+
+function useTimeline(): TimelineContextData {
+	const context = useContext(TimelineContext);
+
+	if (!context) {
+		throw new Error('useTimeline must be used within an AuthProvider');
+	}
+
+	return context;
+}
+
+export { TimelineProvider, useTimeline }
