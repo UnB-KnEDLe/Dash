81dd6ec5220b9247737f5370d2a822395aab855f
jonatas1n <jonatasgeek@gmail.com>
2022-12-01 18:45:44 -0300

    Add modal to results

--- src/templates/timeline/Results/index.tsx
+++ src/templates/timeline/Results/index.tsx
@@ -5,56 +5,34 @@ import { useTimeline } from '../../../hooks/timeline'
 import { Act } from '../../../shared/interfaces/timeline.interface';
 import SmallText from '../../../components/Typography/SmallText';
 import { useCallback, useState } from 'react';
-import {
-  Modal,
-  ModalOverlay,
-  ModalContent,
-  ModalHeader,
-  ModalFooter,
-  ModalBody,
-  ModalCloseButton,
-  useDisclosure
-} from '@chakra-ui/react'
+import { useDisclosure, Box } from '@chakra-ui/react';
+import ResultModal from './ResultModal'
 
 export default function Results() {
   const { acts } = useTimeline();
-  // const [selectedAct, setSelectedAct] = useState<Act|undefined>(undefined);
-  // const { isOpen, onOpen, onClose } = useDisclosure()
+  const [selectedAct, setSelectedAct] = useState<Act|undefined>(undefined);
+  const { isOpen, onOpen, onClose } = useDisclosure()
 
   if (!acts) {
     return (
       <></>
     )
   }
+
   const makeShort = useCallback( (text: string) => {
-    return text;
     if (text.length <= 220) return text;
     return text.slice(0,219) + '...'
   }, []);
 
-  // const handleAct = useCallback( (act: Act) => {
-  //   if (isOpen) {
-  //     setSelectedAct(undefined);
-  //     onClose();
-  //     return;
-  //   };
-    
-  //   setSelectedAct(act);
-  //   onOpen();
-  // }, [])
+  const handleAct = useCallback( (act: Act) => {
+    console.log("jonatas")
+    onOpen();
+    setSelectedAct(act);
+  }, [])
 
   return (
     <VerticalTimeline>
-      {/* {selectedAct ? <Modal isOpen={isOpen} onClose={onClose}>
-        <ModalOverlay />
-        <ModalContent>
-          <ModalHeader>{selectedAct.datePublication}</ModalHeader>
-          <ModalBody>
-            <SmallText smallText={selectedAct.type} />
-            {selectedAct.text}
-          </ModalBody>
-        </ModalContent>
-      </Modal> : null } */}
+      {isOpen ? <ResultModal isOpen={isOpen} selectedAct={selectedAct} onClose={onClose} /> : null }
       { acts?.map( (act, index) => (
         <VerticalTimelineElement
           className="vertical-timeline-element--work"
@@ -64,10 +42,12 @@ export default function Results() {
           iconStyle={{ background: '#2980B9', color: '#fff' }}
           icon={<FcDocument color='#2980B9'/>}
           key={index}
-          // onClick={handleAct(act)}
+          
         >
-          <SmallText smallText={act.type} />
-          <SmallText fontSize='1rem' fontWeight='400' smallText={makeShort(act.text)} />
+          <Box onClick={() => handleAct(act)} _hover={{cursor: "pointer"}}>
+            <SmallText smallText={act.type}/>
+            <SmallText fontSize='1rem' fontWeight='400' smallText={makeShort(act.text)} />
+          </Box>
         </VerticalTimelineElement>
       ) ) }
     </VerticalTimeline>
