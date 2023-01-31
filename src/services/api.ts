93d4314f62d6e97494abe20b0f2edde8b335db87
jonatas1n <jonatasgeek@gmail.com>
2023-01-31 18:09:20 -0300

    set env file

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://knedash.unb.br/dodfminner/api',
+  baseURL: process.env.BASE_URL,
+  // baseURL: 'http://knedash.unb.br/dodfminner/api',
 });
 
 
